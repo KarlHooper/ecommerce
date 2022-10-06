ffa38f467b84ba70bf094e93f5d7bc373c771861
KarlHooper <84038539+KarlHooper@users.noreply.github.com>
2022-10-06 03:21:37 +0000

    Created an admin dashboard page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,19 @@
-<x-app-layout>
-
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- admin sidebar section -->
+      @include('admin.sidebar')
+      <!-- admin header section -->
+      @include('admin.header')
+        <!-- admin body section -->
+        @include('admin.body')
+    <!-- plugins:js -->
+    @include('admin.js')
+    <!-- End custom js for this page -->
+  </body>
+</html>
